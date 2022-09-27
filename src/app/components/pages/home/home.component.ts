1829b621f31c18ea91f8b541b533f214408decac
LucasRosa <lucas.rosa@ipam.org.br>
2022-09-27 11:00:59 -0300

    pegando dados do banco

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+import { MomentService } from 'src/app/services/moment.service';
+import { Moment } from 'src/app/interfaces/Moments';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = environment.baseAPiUrl;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private momentService: MomentService) {
+    this.momentService.getMoments().subscribe((items) => {
+      const { data } = items;
+      console.log('🚀 data', data);
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString(
+          'pt-br'
+        );
+      });
+      this.allMoments = data;
+      this.moments = data;
+    });
   }
 
+  ngOnInit(): void {}
 }
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Moment } from '../interfaces/Moments';
+import { Response } from '../interfaces/Response';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -13,6 +14,10 @@ export class MomentService {
   private apiUrl = `${this.baseApiUrl}api/moments`;
 
   constructor(private http: HttpClient) {}
+
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
 
   createMoment(formaData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formaData);
